c3b455e82857b12229753e12a36b56a8c104f36e
daviro <daviroeik@gmail.com>
2016-09-13 17:03:27 -0400

    index php
    indice de archivos de prueba

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pruebas</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+        <?php
+            echo 'index php';
+        ?>
+    </body>
+</html>
